52bdf51ab082ec92afc1517fe240d9cf6f57c2c0
Yoonhee Son <dev_uni77@gmail.com>
2025-02-15 17:39:59 +0900

    2nd revision

---
+++ parallax.js
@@ -0,0 +1,14 @@
+import SimpleParallax from "simple-parallax-js";
+
+const imageElement = document.getElementById('main-image');
+new simpleParallax(imageElement,{
+    sacle: 2.6,
+    delay:0.3,
+}
+
+);
+
+
+
+// var image = document.getElementsByClassName('thumbnail');
+// new simpleParallax(image);
